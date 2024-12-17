a010fa1e7277c939a560491aa44e6dd123c4490c
ZhannaSerebrennikov <jserebrennikov@gmail.com>
2024-12-17 11:17:00 +0200

    Communication project or simmulation rf communication was added

---
+++ Communication/RFCommunication.cpp
@@ -0,0 +1,30 @@
+#include "RFCommunication.h"
+
+MessageQueue RFCommunication::m_messageQueue;
+
+void RFCommunication::SendPacket(const MessagePacket& message)
+{
+	
+	//std::cout << "RF Communication: Sending Data - " << message << std::endl;
+	m_messageQueue.Enqueue(message);
+}
+
+MessagePacket RFCommunication::ReceivePacket()
+{
+	//std::string data = "Sensor triggered message";
+	//std::cout << "RF Communication : Receiving data - " << data << std::endl;
+	if (!m_messageQueue.IsEmpty())
+	{
+		return m_messageQueue.Dequeue();
+	}
+}
+
+bool RFCommunication::HasMessage()
+{
+	return !m_messageQueue.IsEmpty();
+}
+
+int RFCommunication::GetMessageDstMacAdress()
+{
+	return m_messageQueue.GetQueueDstMacAddress();
+}
---
+++ Communication/RFCommunication.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "../DataHelper/MessageQueue.h"
+
+//In an actual system, RF communication would involve sending and receiving data packets between devices and the control panel. Here, we simulate RF communication with simple method calls
+
+class RFCommunication {
+public:
+	static void SendPacket(const MessagePacket& message);
+	static MessagePacket ReceivePacket();
+	static bool HasMessage();
+	static int GetMessageDstMacAdress();
+
+private:
+	static MessageQueue m_messageQueue;
+};
